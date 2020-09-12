dc88438b2ac4a46bd782a998140183f19eaeb682
gomtang3274 <52527817+gomtang3274@users.noreply.github.com>
2020-09-12 16:10:00 +0900

    10869번 사칙연산

---
+++ 단계별로 풀어보기/1.입출력과 사칙연산/bj10869.cpp
@@ -0,0 +1,15 @@
+//백준 10869번 사칙연산
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    cout << a-b << endl;
+    cout << a*b << endl;
+    cout << a/b << endl;
+    cout << a%b << endl;
+    
+    return 0;
+}
