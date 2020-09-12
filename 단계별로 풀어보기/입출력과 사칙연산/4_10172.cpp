2ee0147b29fa1a8f28be3de6b97944d3bf9a63b0
gomtang3274 <52527817+gomtang3274@users.noreply.github.com>
2020-09-12 15:10:22 +0900

    Create 4_10172.cpp

---
+++ 단계별로 풀어보기/입출력과 사칙연산/4_10172.cpp
@@ -0,0 +1,11 @@
+//백준 10172번 개
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+}
