5fdb351b1897e4915d7ba0e3a32d71b746ec77bc
gomtang3274 <52527817+gomtang3274@users.noreply.github.com>
2020-09-17 14:03:57 +0900

    2753번 윤년

---
+++ 단계별로 풀어보기/2.if문/bj2753.cpp
@@ -0,0 +1,15 @@
+//백준 2753번 윤년
+#include <iostream>
+using namespace std;
+
+int main(){
+    int year;
+    cin >> year;
+    if((year%4==0 && year%100!=0) || year%400==0){
+        cout << 1 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
+    return 0;
+}
